f4f257cd2f4b228d26eaad89a0272dc63b97e941
maxik20192006max@gmail.com <maxik20192006max@gmail.com>
2024-07-13 23:06:12 +0300

    Category Create Page (unfinished)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
-import { Register } from './components/RegisterPage/Register';
-import { Login } from './components/LoginPage/Login';
+// import { Register } from './components/RegisterPage/Register';
+// import { Login } from './components/LoginPage/Login';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { CreateCategoryPage } from './pages/CreateCategoryPage';
 import './App.css'
 
 function App() {
@@ -8,8 +9,9 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          {/* <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} /> */}
+          <Route path="/create-category" element={<CreateCategoryPage />} />
         </Routes>
       </Router>
     </>
