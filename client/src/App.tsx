4dd302405ca4bc483a91683cd261d632c43f2a58
yrakhwas <yrakhwas@gmail.com>
2024-07-29 16:57:54 +0300

    Resolve unconnected component problem

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,3 @@
-import { Register } from './components/RegisterPage/Register';
-import { Login } from './components/LoginPage/Login';
 import ComponentsPreview from './ComponentsPreview';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css'
@@ -9,8 +7,6 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/components-preview" element={<ComponentsPreview/>}/>
         </Routes>
       </Router>
