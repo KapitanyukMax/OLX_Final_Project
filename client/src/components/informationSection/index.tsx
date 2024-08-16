e1b4cd47f11c2effe81447dcb7bb4f8478298a73
Святослав Самолюк <samoliuk.sviatoslav@gmail.com>
2024-08-16 22:04:12 +0300

    CU-86bzuyhcx - Information section. Corrected

--- client/src/components/informationSection/index.tsx
+++ client/src/components/informationSection/index.tsx
@@ -10,70 +10,70 @@ const InformationSection: React.FC = () => {
                 display: 'flex',
                 flexDirection: 'row',
                 width: '1382px',
-                padding:'60px 78px',
+                padding: '60px 78px',
                 justifyContent: 'center',
-                alignItems:'center',
-                gap:'148px',
+                alignItems: 'center',
+                gap: '148px',
                 backgroundColor: 'var(--dark-blue)',
             }}>
                 <Box
-                sx={{
-                    width:'396px',
-                    height: '82px',
-                    textAlign:'left'
-                }}>
+                    sx={{
+                        width: '396px',
+                        height: '82px',
+                        textAlign: 'left'
+                    }}>
                     <Typography className="mainHeaderText">Чому варто розміщувати оголошення саме на DDX?</Typography>
                 </Box>
                 <Box sx={{
-                    display:'flex',
-                    width:'679px',
-                    flexDirection:'column',
-                    alignItems:'flex-start',
+                    display: 'flex',
+                    width: '679px',
+                    flexDirection: 'column',
+                    alignItems: 'flex-start',
                     gap: '64px',
-                    flexShrink:'0',
+                    flexShrink: '0',
                 }}>
                     <Box
-                    sx={{
-                        display:'flex',
-                        flexDirection:'row',
-                        gap:'30px'
-                    }}>
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            gap: '30px'
+                        }}>
                         <Box sx={{
-                            width:'306px',
-                            textAlign:'left'
+                            width: '306px',
+                            textAlign: 'left'
                         }}>
                             <Typography className="headerText" >1. Безкоштовне розміщення оголошень: </Typography>
                             <Typography className="contentText">Базове розміщення оголошень на DDX є безкоштовним, що дозволяє вам економити кошти на рекламі та маркетингу</Typography>
                         </Box>
                         <Box sx={{
-                        width:'306px',
-                        textAlign:'left'
-                    }}>
-                        <Typography className="headerText">3. Безпека та надійність: </Typography>
-                        <Typography className="contentText">DDX використовує сучасні технології захисту даних, щоб забезпечити безпечне спілкування та транзакції між користувачами. Ваші особисті дані будуть надійно захищені</Typography>
-                    </Box>
+                            width: '306px',
+                            textAlign: 'left'
+                        }}>
+                            <Typography className="headerText">3. Безпека та надійність: </Typography>
+                            <Typography className="contentText">DDX використовує сучасні технології захисту даних, щоб забезпечити безпечне спілкування та транзакції між користувачами. Ваші особисті дані будуть надійно захищені</Typography>
+                        </Box>
                     </Box>
                     <Box
-                    sx={{
-                        display:'flex',
-                        flexDirection:'row',
-                        gap:'30px'
-                    }}>
-                    
-                    <Box sx={{
-                            width:'306px',
-                            textAlign:'left'
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            gap: '30px'
+                        }}>
+
+                        <Box sx={{
+                            width: '306px',
+                            textAlign: 'left'
                         }}>
                             <Typography className="headerText">2. Різноманітність категорій: </Typography>
-                            <Typography className="contentText">На DDX представлений широкий спектр категорій, що дозволяє розміщувати оголошення про продаж різноманітних товарів та послуг —від автомобілів та нерухомості до електроніки та одягу</Typography>
+                            <Typography className="contentText">На DDX представлений широкий спектр категорій, що дозволяє розміщувати оголошення про продаж різноманітних товарів та послуг — від автомобілів та нерухомості до електроніки та одягу</Typography>
                         </Box>
-                    <Box sx={{
-                        width:'306px',
-                        textAlign:'left'
-                    }}>
-                        <Typography className="headerText">4. Географічна прив'язка: </Typography>
-                        <Typography className="contentText">Дозволяє користувачам шукати товари та послуги в своєму регіоні, що зручно для локальних угод. Ви можете легко знайти пропозиції поблизу свого місцезнаходження</Typography>
-                    </Box>
+                        <Box sx={{
+                            width: '306px',
+                            textAlign: 'left'
+                        }}>
+                            <Typography className="headerText">4. Географічна прив'язка: </Typography>
+                            <Typography className="contentText">Дозволяє користувачам шукати товари та послуги в своєму регіоні, що зручно для локальних угод. Ви можете легко знайти пропозиції поблизу свого місцезнаходження</Typography>
+                        </Box>
                     </Box>
                 </Box>
             </Box>
