8999a92cc0d9f9e9368b9ce592435f33ae9f02c7
KapitanyukMax <maxkapitaniuk@gmail.com>
2024-09-12 21:20:46 +0300

    Minor changes, resolving conflicts after merge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import ComponentsPreview from './ComponentsPreview';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AdvertCreatePage } from './pages/AdvertCreatePage';
-import { AuthModal } from './pages/RegistrationPage';
+import { AuthModal } from './pages/RegistrationPage/AuthModal';
 import { AdvertPage } from './pages/AdvertPage';
 import './App.css';
 
