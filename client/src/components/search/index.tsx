e4e1071ebb652e0b8d1c3737cf9fd274158b196c
KapitanyukMax <maxkapitaniuk@gmail.com>
2024-10-06 00:30:38 +0300

    CU-86c0jcg2z - Search without cities

---
+++ client/src/components/search/index.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import { StyledInput } from '../input';
+import StyledButton from '../button';
+import { Box, Input, InputAdornment } from '@mui/material';
+import './styles.css';
+
+interface SearchProps {
+    value?: string,
+    onSearchTermChange: (searchTerm: string) => void,
+    onSearch: (searchTerm: string) => void
+}
+
+const Search: React.FC<SearchProps> = ({ value, onSearch, onSearchTermChange }) => {
+    const StyledSearchIcon: React.FC = () => (
+        <SearchIcon sx={{ color: "black" }} />
+    );
+
+    const [searchTerm, setSearchTerm] = useState(value ?? '');
+
+    return (
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: '60px',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+        }}>
+            <Box sx={{
+                width: '522px',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                textAlign: 'left'
+            }}>
+                <Input value={searchTerm} placeholder={'Що шукаємо?'} className='basic-input'
+                    onChange={e => {
+                        setSearchTerm(e.target.value);
+                        onSearchTermChange(e.target.value);
+                    }}
+                    endAdornment={(
+                    <InputAdornment position='end'>
+                        <StyledSearchIcon />
+                    </InputAdornment>)} />
+            </Box>
+            <StyledInput value='Вся Україна' iconEnd={StyledSearchIcon} width="254px" />
+            <StyledInput value='Місто/село' iconEnd={StyledSearchIcon} width="254px" />
+            <StyledButton text='Пошук' type='contained' primaryColor='var(--dark-blue)' hoverColor='black' className='button-small' onClick={() => onSearch(searchTerm)} />
+        </Box>
+    );
+};
+
+export {
+    Search
+};
